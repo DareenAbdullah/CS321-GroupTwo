35097ebc119bf085b07254fa707ef1ad5a6d9e4f
Romel Munoz Valencia <57759727+Rmmvx@users.noreply.github.com>
2023-02-21 14:21:07 -0500

    Update webcam_test.py

--- python/webcam_test.py
+++ python/webcam_test.py
@@ -29,24 +29,39 @@ run(setup, loop)
 '''
 #!/usr/bin/env python3
 import cv2 as cv
+import imutils
+import socket
+import time
+import base64
+import numpy as np
 
+BUFF_SIZE = 65536
+
+#Opens socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+port = 12345
+
+#Opens default camera
+#otherwise, we should we be able to use cap.open(path) after VideoCapture() object is created
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    # Our operations on the frame come here
+	print("Cannot open camera")
+	exit()
 
-    # Display the resulting frame
-    #cv.imshow('Video', frame)
-    #if cv.waitKey(1) == ord('q'):
-     #   break
-# When everything done, release the capture
+while True:
+	while (cap.isOpened()):
+		#Captures image
+		ret, frame = cap.read()
+
+		if not ret:
+			print ("Can't receive frame. Exiting ...")
+			break
+
+		encoded, buffer = cv.imencode('.jpg', frame, [cv.IMWRITE_JPEG_QUALITY, 80])
+
+		#Sends packet to an IP address, and port number
+		server_socket.sendto(buffer.tobytes(), (socket.gethostname(), port))
+
+#closes webcam		
 cap.release()
 '''
