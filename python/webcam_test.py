55f5e0663d3f8e81095c47cb6921bbfa6f0936ad
Romel Munoz Valencia <57759727+Rmmvx@users.noreply.github.com>
2023-02-21 14:46:59 -0500

    Update webcam_test.py

--- python/webcam_test.py
+++ python/webcam_test.py
@@ -27,6 +27,12 @@ def loop():
   
 run(setup, loop)
 '''
+# ON MAC, the following command needs to be ran
+# sudo sysctl -w net.inet.udp.maxdgram=6553500
+# data sent is currently too big, my next goal is to find out how
+# to reduce this to keep it under the MAX SIZE LIMIT
+# it also currently runs endlessly
+
 #!/usr/bin/env python3
 import cv2 as cv
 import imutils
