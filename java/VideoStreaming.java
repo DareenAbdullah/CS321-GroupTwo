da354e4a8ca8f4b1278306c19cd53f583e48dadc
Romel Munoz Valencia <57759727+Rmmvx@users.noreply.github.com>
2023-03-02 22:54:03 -0500

    Add files via upload

--- java/VideoStreaming.java
+++ java/VideoStreaming.java
@@ -5,13 +5,6 @@ import java.awt.FlowLayout;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
-/**
-* This class receives packets sent to port 12345
-* It then decodes the data into an image and displayes it
-* ie. "streams" incoming images
-*
-* It currently goes on forever, even when it is not receiving anything.
-*/
 public class VideoStreaming{
     public static void main(String[] args){
         //Sets the frame to display video
@@ -19,15 +12,10 @@ public class VideoStreaming{
         nFrame.setLayout(new FlowLayout());
         nFrame.setSize(1280,720);
         JLabel lbl = new JLabel();
-
-        //Supposed to break the loop when it hits 0, used for testing
-        //But it could be used to end the program after x seconds of not
-        //receiving any packets.
+        
         int timer = 10000;
-        try{
+        try(DatagramSocket socket = new DatagramSocket(12345)){
             System.out.println("Starting");
-            DatagramSocket socket = new DatagramSocket(12345);
-
             //Currently, endless loop
             while(timer != 0){
                 byte[] buffer = new byte[65536];
@@ -43,7 +31,8 @@ public class VideoStreaming{
 
                 nFrame.setVisible(true);
                 nFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                timer--; 
+                timer--;
+                System.out.println(timer);
             }
         }
         catch(SocketException e){
