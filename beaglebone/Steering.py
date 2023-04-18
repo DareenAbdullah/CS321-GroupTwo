91f51a9832adcc67525b0f68636e130379ba568d
Romel Munoz Valencia <57759727+Rmmvx@users.noreply.github.com>
2023-04-18 16:05:54 -0400

    Add files via upload

---
+++ beaglebone/Steering.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import Adafruit_BBIO.PWM as PWM
+
+class Steering:
+    #Initialize pins, start servo, and set cycle limits
+    def __init__(self):
+        self.servoPin = 'P9_14'
+        self.currentPosition = 5
+        PWM.start(self.servoPin, 5, 50)
+    
+    def setCycleLeft(self):
+        if self.currentPosition > 5:
+            self.currentPosition -= 1
+            PWM.set_duty_cycle(self.servoPin, self.currentPosition)
+        
+    def setCycleRight(self):
+        if self.currentPosition < 10:
+            self.currentPosition += 1
+            PWM.set_duty_cycle(self.servoPin, self.currentPosition)
