370732738a53ea3154e2f6ac6961a84d9fa3db28
Romel Munoz Valencia <57759727+Rmmvx@users.noreply.github.com>
2023-04-25 11:17:44 -0400

    Update Steering.py
    steering

--- beaglebone/Steering.py
+++ beaglebone/Steering.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 import Adafruit_BBIO.PWM as PWM
 
+SERVO = 'P9_14'
+DEFAULT_POSITION = 7.5
+FULL_LEFT_POS = 5
+FULL_RIGHT_POS = 10
+
 class Steering:
     #Initialize pins, start servo, and set cycle limits
     def __init__(self):
-        self.servoPin = 'P9_14'
-        self.currentPosition = 5
-        PWM.start(self.servoPin, 5, 50)
-    #Moves servo left
+        self.currentPosition = DEFAULT_POSITION
+        PWM.start(self.servoPin, DEFAULT_POSITION, 50)
+    
     def setCycleLeft(self):
-        if self.currentPosition > 5:
-            self.currentPosition -= 1
+        if self.currentPosition > FULL_LEFT_POS:
+            self.currentPosition -= 0.5
             PWM.set_duty_cycle(self.servoPin, self.currentPosition)
-    #Moves servo right 
+        
     def setCycleRight(self):
-        if self.currentPosition < 10:
-            self.currentPosition += 1
+        if self.currentPosition < FULL_RIGHT_POS:
+            self.currentPosition += 0.5
             PWM.set_duty_cycle(self.servoPin, self.currentPosition)
+
+#TODO: find a way to reset position of the wheels when direction keys are released.
