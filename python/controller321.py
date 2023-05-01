9a9c33c4ccb7c60a7828c5ba313265ec0b59b597
afroraydude <thomasray0@gmail.com>
2023-05-01 11:13:58 -0400

    yes

--- python/controller321.py
+++ python/controller321.py
@@ -1,4 +1,6 @@
 import pygame
+import socket
+import threading
 
 # Initialize Pygame
 pygame.init()
@@ -20,6 +22,50 @@ controller.init()
 # Print the controller's name
 print("Connected to controller:", controller.get_name())
 
+# Initialize a socket for TCP communication
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Specify the server's address and port
+server_address = ('localhost', 12345)  # Replace with your server's address and port
+
+# Bind the socket to the server's address and port
+s.bind(server_address)
+
+# Listen for incoming connections
+s.listen(1)
+
+# List of connected clients
+clients = []
+
+# Function to handle incoming connections
+def handle_client(client_socket):
+    while True:
+        # Receive data from the client
+        data = client_socket.recv(1024)
+        if not data:
+            clients.remove(client_socket)
+            break
+
+    client_socket.close()
+
+# Start a thread that listens for incoming connections
+def start_server():
+    while True:
+        print("Waiting for a connection...")
+        client_socket, client_address = s.accept()
+        print("Accepted connection from:", client_address)
+        
+        # Add the new client to the list of clients
+        clients.append(client_socket)
+        
+        # Start a new thread to handle this client's communication
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread.start()
+
+# Start the server thread
+server_thread = threading.Thread(target=start_server)
+server_thread.start()
+
 # Main game loop
 running = True
 while running:
@@ -33,47 +79,22 @@ while running:
             # Handle button press events
             if event.button == 0:
                 print("A button was pressed")
-            if event.button == 1:
-                print("B button was pressed")
-            if event.button == 3:
-                print("Y button was pressed")
-
-            if event.button == 2:  
-                print("X button was pressed, exiting program")
-                running = False
+                message = "A button was pressed"
+                for client in clients:
+                    client.sendall(message.encode())
+            # Handle other button events...
 
         elif event.type == pygame.JOYBUTTONUP:
             # Handle button release events
-            if event.button == 0:
-                print("A button was released")
-            if event.button == 1:
-                print("B button was released")
-            if event.button == 3:
-                print("Y button was released")
+            # Handle these events...
 
-        elif event.type == pygame.JOYAXISMOTION:
+        #elif event.type == pygame.JOYAXISMOTION:
             # Handle joystick motion events
-            if event.axis == 0:
-                # X-axis motion of the left joystick
-                x_axis_left = event.value
-                print("Left Joystick X-axis value:", x_axis_left)
+            # Handle these events...
+    
 
-            elif event.axis == 1:
-                # Y-axis motion of the left joystick
-                y_axis_left = event.value
-                print("Left Joystick Y-axis value:", y_axis_left)
-
-            elif event.axis == 3:
-                # X-axis motion of the right joystick
-                x_axis_right = event.value
-                print("Right Joystick X-axis value:", x_axis_right)
-
-            elif event.axis == 4:
-                # Y-axis motion of the right joystick
-                y_axis_right = event.value
-                print("Right Joystick Y-axis value:", y_axis_right)
+# Close the socket
+s.close()
 
 # Quit Pygame
 pygame.quit()
-
-
