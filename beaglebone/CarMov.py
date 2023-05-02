2e381d8c04ec63aa8e3ca7a1afcfd2140fc35f67
Romel Munoz Valencia <57759727+Rmmvx@users.noreply.github.com>
2023-05-02 17:03:21 -0400

    Update CarMov.py

--- beaglebone/CarMov.py
+++ beaglebone/CarMov.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-import Adafruit_BBIO as PWM
+import Adafruit_BBIO.PWM as PWM
 
 ESC_MOTOR = 'P9_16'
 #100% forward throttle is 5% cycle
-FULL_THROTTLE = 5
+FULL_THROTTLE = 7.2
 
 #100% reverse is 10% cycle
-REVERSE_THROTTLE = 10
+REVERSE_THROTTLE = 7.7
 
 #Starting position
 RESTING_POSITION = 7.5
@@ -19,19 +19,20 @@ class CarMov:
     
     def move_forward(self):
         if self.current_throttle > FULL_THROTTLE:
-            self.current_throttle -= 0.5
+            self.current_throttle -= 0.01
             PWM.set_duty_cycle(ESC_MOTOR, self.current_throttle)
-        return current_throttle
+        return self.current_throttle
     
     def move_backward(self):
         if self.current_throttle < REVERSE_THROTTLE:
-            self.current_throttle += 0.5
+            self.current_throttle += 0.01
             PWM.set_duty_cycle(ESC_MOTOR, self.current_throttle)
-        return current_throttle
+        return self.current_throttle
     
     def reset_throttle(self):
         while self.current_throttle > RESTING_POSITION:
-            self.current_throttle -= 0.5
+            self.current_throttle -= 0.01
             PWM.set_duty_cycle(ESC_MOTOR, self.current_throttle)
-            
-#TODO: find a way to reset throttle position once key presses are relesed.
+        while self.current_throttle < RESTING_POSITION:
+            self.current_throttle += 0.01
+            PWM.set_duty_cycle(ESC_MOTOR, self.current_throttle)
