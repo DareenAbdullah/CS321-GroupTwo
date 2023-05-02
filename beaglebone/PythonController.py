e38dc6aff888ae39da53b26be2ef8000ee757198
Romel Munoz Valencia <57759727+Rmmvx@users.noreply.github.com>
2023-05-02 11:41:26 -0400

    Update PythonController.py

--- beaglebone/PythonController.py
+++ beaglebone/PythonController.py
@@ -2,17 +2,14 @@
 
 import socket
 import codecs
-import VideoStream
 import threading
 import Steering
 import CarMov
 
-HOST = "127.0.0.1"
-PORT = 12345
-num = 0
+#HOST = "192.168.8.229"
+HOST = "192.168.8.243"
+PORT = 12346
 
-#Initialize VideoStream object
-streamer = VideoStream.VideoStream()
 #thread1 will handle video streaming
 thread1 = None
 
@@ -23,64 +20,50 @@ thread2 = None
 car_throttle = CarMov.CarMov()
 #thread3 will handle acceleration
 thread3 = None
-print("Now waiting for socket to connect")
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	s.bind((HOST, PORT))
-	s.listen()
-	connection, address = s.accept()
-	with connection:
-		print(f"Connected by {address}")
-		while True:
-			data = connection.recv(1024)
-			if not data:
-				continue
-			data = str(data, 'utf-8')
-			if "q" in data:
-				streamer.stop_stream()
+	s.connect((HOST, PORT))
+	s.sendall(b"Hello from Racer2")
+	print('worked')
+	while True:
+		data = s.recv(1024)
+		if not data:
+			continue
+		data = data.decode('utf-8')
+		data = data.split()
+		print(data)
+		#if A, B, Y , or X were pressed
+		if len(data) < 2:
+			if 'X' in str(data[0]):
 				break
-			elif "none" in data:
-				connection.sendall(bytes('none\n', 'utf-8'))
-
-			elif "0" in data:
-				connection.sendall(bytes('0\n', 'utf-8'))
-				steer.setCycleLeft()
-				print("Running moveLeft()")
-				
-			elif "1" in data:
-				connection.sendall(bytes('1\n', 'utf-8'))
-				steer.setCycleRight()
-				print("Running moveRight()")
-				
-			elif "2" in data:
-				connection.sendall(bytes('2\n', 'utf-8'))
-				car_throttle.move_forward()
-				print("Running moveForward()")
-				
-			elif "3" in data:
-				connection.sendall(bytes('3\n', 'utf-8'))
-				car_throttle.move_backward()
-				print("Running moveInReverse()")
-				
-			elif "4" in data:
-				if streamer.is_streaming():
-					connection.sendall(bytes('Streaming\n', 'utf-8'))
-					print('streamer is streaming')
+			continue
+		else:
+			if 'L' in data[0][0] or 'R' in data[0][0]:
+				#Makes the second argumetn (axix value) numeric
+				print(data[1])
+				axis = float(data[1][:3])
+				if axis == 0:
+					if 'L' in data[0][0]:
+						steer.resetCycle()
+						print(data[0] + str(axis))
+					if 'R' in data[0][0]:
+						car_throttle.reset_throttle()
+						print(data[0] + str(axis))
 				else:
-					print('streamer is NOT streaming')
-					streamer.setup_stream()
-					thread1 = threading.Thread(target=streamer.start_stream, args=(socket.gethostname(), 12346))
-					thread1.start()
-					connection.sendall(bytes('Streaming\n', 'utf-8'))
-			elif "5" in data:
-				print('here!')
-				if streamer.is_streaming():
-					streamer.stop_stream()
-					connection.sendall(bytes('Not streaming\n', 'utf-8'))
-				else:	
-					connection.sendall(bytes('Not streaming\n', 'utf-8'))
+					if 'LX:' in data[0]:
+						if axis < 0:
+							steer.setCycleLeft()
+							print(data[0] + str(axis))
+						else:
+							steer.setCycleRight()
+							print(data[0] + str(axis))
+					if 'RY:' in data[0]:
+						if axis < 0:
+							car_throttle.move_backward()
+							print(data[0] + str(axis))
+						else:
+							car_throttle.move_backward()
+							print(data[0] + str(axis))
 				
-			else:
-				connection.sendall(bytes("\n", 'utf-8'))
-				
-
+			
 print("Connection successful from beaglebone")
